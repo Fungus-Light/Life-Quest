e29df4778343bf003417eb20cbed41b67bebd40d
ZZN <542461670@qq.com>
2018-10-26 01:40:39 +0800

    make the app runnable

--- app/main.js
+++ app/main.js
@@ -1,22 +1,78 @@
 const electron =require('electron');
 const url =require('url');
 const path=require('path');
+//needed libraries
+const{app,BrowserWindow,Menu}=electron;//app means the main process
+//Menu means the tool bar,BrowserWindow means 
 
-const{app,BrowserWindow}=electron;
+process.env.NODE_ENV='develop'//mark the running edition
 
-process.env.NODE_ENV='develop'
+let mainWindow;//the main window object
 
-let mainWindow;
-
-app.on('ready',function(){
-    mainWindow=new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'mainWindow.html'),
+/*------------Some Functions--------------*/
+function FormatPathToURL(_path)//this format the path of mainWindow.html into URL address,and 
+{
+    return url.format({
+        pathname: path.join(__dirname,_path),
         protocol: 'file:',
         slashes: true
-    }));
+    })
+}
 
+
+/*----------------------------------------*/
+
+//main logic area
+app.on('ready',function(){
+    mainWindow=new BrowserWindow({});
+    mainWindow.loadURL(FormatPathToURL("mainWindow.html"));//this load the mainWindow page
     mainWindow.on('closed',function(){
         app.quit();
-    });
+    });//if mainWindow is closed,end the application.
+
+    const mainMenu=Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);//set the mainWindow tool bar style
 });
+
+/*--------Menu Style Define Area---------*/
+
+const NullMenuTemplate=[{
+}];//the style of window which has no tool bar
+
+const mainMenuTemplate =//default mainWindow tool bar style
+[
+{
+    label: 'File',
+    submenu:[
+        {
+            label: 'Quit',//the menu buttom to quit the app
+            accelerator: process.platform=='darwin'?'Command+Q':'Ctrl+Q',
+            click(){
+                app.quit();
+            }
+        }
+    ]
+}
+];
+
+if(process.platform=='darwin'){
+    mainMenuTemplate.shift({});
+}//the function to ensure the style shown normally on MacOs
+
+if(process.env.NODE_ENV != 'production')//open Developer Tools when not in production mode
+{
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[{
+            accelerator: process.platform=='darwin'?'Command+I':'Ctrl+I',//accelerator keys
+            label: 'Toggle DevTools',
+            click(item,focusedWindow){
+                focusedWindow.toggleDevTools();
+            }
+        },
+    {
+        role: 'reload'
+    }]
+    });
+}
+/*---------------------------------------*/
