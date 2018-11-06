21e071564b7f26be08c69cbbd3b2b1aa4297774e
FungusFlowchart <542461670@qq.com>
2018-11-06 15:57:51 +0800

    two windows

--- app/app.js
+++ app/app.js
@@ -20,13 +20,13 @@ function FormatPathToURL(_path)//this format the path of mainWindow.html into UR
     })
 }
 
-function CreatSizedWindow(w,h,hasFrame,_resizable,isShow){
-    return new BrowserWindow({width:w,height:h,frame: hasFrame,resizable: _resizable,show: isShow})
+function CreatSizedWindow(w,h,hasFrame,_resizable,isShow,_parent){
+    return new BrowserWindow({width:w,height:h,frame: hasFrame,resizable: _resizable,show: isShow,parent:_parent})
 }
 
 //main logic area
 app.on('ready',function(){
-    mainWindow=CreatSizedWindow(1280,720,false,true,true)
+    mainWindow=CreatSizedWindow(1280,720,false,true,true,null)
     mainWindow.loadURL(FormatPathToURL("./mainWindow.html"));//this load the mainWindow page
     //if mainWindow is closed,end the application.
     mainWindow.on('closed',function(){
@@ -44,15 +44,22 @@ ipcMain.on('forcequit',function(){
 
 ipcMain.on('connectus',function(){
     if(connectWindow==null){
-        connectWindow=CreatSizedWindow(300,300,false,true,true);
+        connectWindow=CreatSizedWindow(300,300,false,true,true,mainWindow);
         connectWindow.loadURL(FormatPathToURL("./windows/connectUs.html"));
         connectWindow.on('closed',function(){
-            connectWindow=null;
+        connectWindow=null;
         });
     }
 });
 
 ipcMain.on("questlib",function(){
+    if(questlibWindow==null){
+        questlibWindow=CreatSizedWindow(960,540,false,false,true,mainWindow);
+        questlibWindow.loadURL(FormatPathToURL("./windows/QuestLibrary.html"));
+        questlibWindow.on('closed',function(){
+        questlibWindow=null;
+        });
+    }
     
 })
 
---
+++ app/scripts/questlib.js
@@ -0,0 +1,13 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+    instances.open();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+    instances.open();
+  });
