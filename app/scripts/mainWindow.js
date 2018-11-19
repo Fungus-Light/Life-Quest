826cbe23761185e70f25ed7ce2311287238bd4df
FungusFlowchart <542461670@qq.com>
2018-11-19 11:49:17 +0800

    add jQuery

--- app/scripts/mainWindow.js
+++ app/scripts/mainWindow.js
@@ -1,42 +1,48 @@
-const electron =require('electron');
-const {ipcRenderer}=electron;
+// const electron =require('electron');
+// const {ipcRenderer}=electron;
 
 
-const quitBtn=document.getElementById("quit-btn");
-quitBtn.addEventListener('click',ForceQuit);
-function ForceQuit(){
-    ipcRenderer.send('forcequit');
-}
 
-const connectBtn=document.getElementById("connect-btn");
-connectBtn.addEventListener('click',ConnectUs);
-function ConnectUs(){
-    ipcRenderer.send('connectus');
-}
+// const quitBtn=document.getElementById("quit-btn");
+// quitBtn.addEventListener('click',ForceQuit);
+// function ForceQuit(){
+//     ipcRenderer.send('forcequit');
+// }
 
-const questlibbtn=document.getElementById("quest-library");
-questlibbtn.addEventListener('click',QuestLib);
-function QuestLib(){
-    ipcRenderer.send("questlib");
-}
+// const connectBtn=document.getElementById("connect-btn");
+// connectBtn.addEventListener('click',ConnectUs);
+// function ConnectUs(){
+//     ipcRenderer.send('connectus');
+// }
 
-const shoppingbtn=document.getElementById("shopping");
-shoppingbtn.addEventListener("click",function(){
-    ipcRenderer.send("shopping");
-})
+// const questlibbtn=document.getElementById("quest-library");
+// questlibbtn.addEventListener('click',QuestLib);
+// function QuestLib(){
+//     ipcRenderer.send("questlib");
+// }
 
-// const testwin=document.getElementById("testwin");
-// testwin.addEventListener("click",function(){
-//     ipcRenderer.send("testwin");
+// const shoppingbtn=document.getElementById("shopping");
+// shoppingbtn.addEventListener("click",function(){
+//     ipcRenderer.send("shopping");
+// })
+
+// // const testwin=document.getElementById("testwin");
+// // testwin.addEventListener("click",function(){
+// //     ipcRenderer.send("testwin");
+// // });
+
+// const personbtn=document.getElementById("personspace");
+// personbtn.addEventListener("click",function(){
+//     ipcRenderer.send("personspace");
 // });
 
-const personbtn=document.getElementById("personspace");
-personbtn.addEventListener("click",function(){
-    ipcRenderer.send("personspace");
-});
+// const elsebtn=document.getElementById("else");
+// elsebtn.addEventListener("click",function(){
+//     ipcRenderer.send("else");
+// })
+// /*....................................................*/
 
-const elsebtn=document.getElementById("else");
-elsebtn.addEventListener("click",function(){
-    ipcRenderer.send("else");
-})
-/*....................................................*/
+
+$( document ).ready(function(){
+    $(".dropdown-trigger").dropdown();
+});
