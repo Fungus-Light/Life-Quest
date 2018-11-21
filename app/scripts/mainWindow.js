8a595be81a6e40e16dde269abfe0fd605eeba8be
ZZN <542461670@qq.com>
2018-11-21 16:34:50 +0800

    add a test win and make code depart

--- app/scripts/mainWindow.js
+++ app/scripts/mainWindow.js
@@ -1,23 +1,3 @@
-const electron = require('electron');
-const { ipcRenderer } = electron;
-M.AutoInit();
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  var setting = document.getElementById("setting");
-  var instances = M.Sidenav.init(setting, {
-    edge: 'left'
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  var personspace = document.getElementById("personspace");
-  var instances = M.Sidenav.init(personspace, {
-    edge: 'left'
-  });
-});
-
-
 const quitBtn = document.getElementById("quit-btn");
 quitBtn.addEventListener('click', ForceQuit);
 const quitBtn1 = document.getElementById("quit-btn1");
@@ -27,6 +7,7 @@ function ForceQuit() {
   ipcRenderer.send('forcequit');
 }
 
-$(document).ready(function () {
-  $(".dropdown-trigger").dropdown();
+const testbtn=document.getElementById("developfunct");
+testbtn.addEventListener('click',function(){
+  ipcRenderer.send('testwin');
 });
