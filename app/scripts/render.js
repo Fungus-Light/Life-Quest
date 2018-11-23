f486f3a4caa9b6572b38bfdd5b1dfc934a73d482
ZZN <542461670@qq.com>
2018-11-23 17:41:46 +0800

    add two sidenav

--- app/scripts/render.js
+++ app/scripts/render.js
@@ -1,19 +1,5 @@
 M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function () {
-  var setting = document.getElementById("setting");
-  var instances = M.Sidenav.init(setting, {
-    edge: 'left'
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-  var personspace = document.getElementById("personspace");
-  var instances = M.Sidenav.init(personspace, {
-    edge: 'left'
-  });
-});
-
 $(document).ready(function () {
     $(".dropdown-trigger").dropdown();
   });
