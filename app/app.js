de3d919b47c798a4e3b328ff812d29afbb232a61
ZZN <542461670@qq.com>
2018-11-24 17:03:55 +0800

    summary and some render code

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
 const electron = require('electron');
+//electron的类
 const url = require('url');
+//url相关功能的类
 const path = require('path');
-
+//路径相关的类
 //needed libraries
+//上面是引入的类
 const { app, BrowserWindow, ipcMain } = electron;//app means the main process
 //BrowserWindow means windowsshoppingWindow
+//三个静态变量，app代表主进程，BrowserWindow代表窗口类，ipcMain是进程通信代表主进程的
 
 process.env.NODE_ENV = 'develop'//mark the running edition
+//运行状态标志
 
 let mainWindow, testwin = null;
+//主窗口、测试窗口的变量
 
 //the window objects
 
@@ -20,11 +26,11 @@ function FormatPathToURL(_path)//this format the path of mainWindow.html into UR
         protocol: 'file:',
         slashes: true
     })
-}
+}//把相对路径转化成url的方法，更不容易出错
 
 function CreatSizedWindow(w, h, hasFrame, _resizable, isShow, _parent) {
     return new BrowserWindow({ width: w, height: h, frame: hasFrame, resizable: _resizable, show: isShow, parent: _parent })
-}
+}//创建指定大小和样式的窗口，参数分别为宽度，高，是否边框，是否可调整大小，是否显示，父级
 
 //main logic area
 app.on('ready', function () {
@@ -34,7 +40,7 @@ app.on('ready', function () {
     mainWindow.on('closed', function () {
         app.quit();
     })
-
+//主进程加载完毕，加载主窗口
 });
 
 
@@ -43,6 +49,7 @@ ipcMain.on('forcequit', function () {
     console.log('quit');
     mainWindow.close();
 });
+//主进程接收到'forcequit'消息，结束进程
 
 ipcMain.on("testwin", function () {
     if (testwin == null) {
@@ -52,6 +59,6 @@ ipcMain.on("testwin", function () {
             testwin = null;
         })
     }
-});
+});//接到'testwin'消息，开启测试窗口
 
 
--- app/scripts/render.js
+++ app/scripts/render.js
@@ -1,5 +1,57 @@
 M.AutoInit();
+var Itenid=0;
 
-$(document).ready(function () {
-    $(".dropdown-trigger").dropdown();
-  });
+const shop_Renderer=document.getElementById("shop_Renderer");
+const shop_addBtn=document.getElementById("addshoplist");
+shop_addBtn.addEventListener("click",AddItem);
+
+function MakeUpElement(_tag,_classname,_innerText){
+  var temp=document.createElement(_tag);
+  temp.className=_classname;
+  temp.innerText=_innerText;
+  return temp;
+}
+
+/*code to render the itemshop*/
+
+/*
+<li class="collection-item avatar">
+    <i class="material-icons circle red">play_arrow</i>
+    <span class="title">Title</span>
+    <p>
+        First Line <br>
+        Second Line
+    </p>
+    <a href="#!" class="secondary-content"><i class="material-icons">stars</i></a>
+</li>
+*/
+function AddItem(){
+  Itenid++;
+  var temp=MakeUpItem("Item","play_arrow",180,"this is description");
+  temp.setAttribute("id","item"+Itenid);
+  shop_Renderer.appendChild(temp);
+}
+
+function MakeUpItem(_title,_icon,_point,_description){
+  var temp=MakeUpElement("li","collection-item avatar","");
+
+  var icon=MakeUpElement("i","material-icons circle red",_icon);
+  var title=MakeUpElement("span","title",_title);
+  var description=MakeUpElement("p","","point"+_point+"<br>"+_description);
+  
+  var buttom=MakeUpElement("a","secondary-content","");
+  buttom.setAttribute("href","#!");
+  var btnicon=MakeUpElement("i","material-icons","insert_emoticon");
+  buttom.appendChild(btnicon);
+
+  temp.appendChild(icon);
+  temp.appendChild(title);
+  temp.appendChild(description);
+  temp.appendChild(buttom);
+
+  return temp;
+}
+
+function ClearAll(RenderId){
+  document.getElementById(RenderId).innerHTML="";
+}
