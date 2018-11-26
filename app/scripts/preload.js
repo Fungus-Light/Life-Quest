e0aeeafb5548176892c8407a6beb33488360be50
FungusFlowchart <542461670@qq.com>
2018-11-26 11:48:37 +0800

    include chartist add some style

--- app/scripts/preload.js
+++ app/scripts/preload.js
@@ -1,5 +1,25 @@
 const electron = require('electron');
 const { ipcRenderer } = electron;
+const fs=require("fs");
+
+function loadfile(){
+    var _data=fs.readFileSync("./app/userdata/questlib.json");
+    console.log("xx"+_data);
+    questlib=JSON.parse(_data);
+}
+
+
+function savefile(e){
+    var jsonstr=JSON.stringify(questlib);
+    console.log(jsonstr);
+    fs.writeFile("./app/userdata/questlib.json", jsonstr,function(err){
+        if (err) {
+            return console.error(err);
+        }
+        console.log("数据写入成功！");
+    })
+}
+/*-------------------------------------------------- */
 
 function singleTask(_name,_rewardCount,_remark,_label,_frequence,_ddl)
 {
--- app/scripts/render.js
+++ app/scripts/render.js
@@ -2,10 +2,15 @@ M.AutoInit();
 var Itenid=0;
 
 const RenderId_shop="shop_Renderer";
+const RenderId_task="task_Renderer";
 
 const shop_Renderer=document.getElementById(RenderId_shop);
+const task_Renderer=document.getElementById(RenderId_task);
+
 const shop_addBtn=document.getElementById("addshoplist");
 shop_addBtn.addEventListener("click",AddItem);
+const task_addbtn=document.getElementById("addtastk");
+task_addbtn.addEventListener('click',AddTask);
 
 function MakeUpElement(_tag,_classname,_innerText){
   var temp=document.createElement(_tag);
@@ -28,34 +33,103 @@ function ClearAll(RenderId){
         First Line <br>
         Second Line
     </p>
-    <a href="#!" class="secondary-content"><i class="material-icons">stars</i></a>
-</li>
+    <div class="secondary-content">
+      <a href="#!" class="waves-effect waves-red btn-flat"><i class="material-icons">stars</i></a>
+      <a href="#!" class="waves-effect waves-red btn-flat"><i class="material-icons">stars</i></a>
+    </div>
+    </li>
 */
 function AddItem(){
   Itenid++;
-  var temp=MakeUpItem("Item","play_arrow",180,"this is description");
+  var temp=MakeUpItem("Item","play_arrow",180,"is description");
   temp.setAttribute("id","item"+Itenid);
   shop_Renderer.appendChild(temp);
 }
 
-function MakeUpItem(_title,_icon,_point,_description){
+function MakeUpItem(_title,_icon,_point,_description)
+{
   var temp=MakeUpElement("li","collection-item avatar","");
 
   var icon=MakeUpElement("i","material-icons circle red",_icon);
   var title=MakeUpElement("span","title",_title);
-  var description=MakeUpElement("p","","point"+_point+"<br>"+_description);
-  
-  var buttom=MakeUpElement("a","secondary-content","");
-  buttom.setAttribute("href","#!");
-  var btnicon=MakeUpElement("i","material-icons","insert_emoticon");
-  buttom.appendChild(btnicon);
+  var des1=MakeUpElement("p","","point"+_point);
+  var des2=MakeUpElement("p","","description"+_description);
+
+  var buttom=MakeUpElement("div","secondary-content","");
+  var buttom1=MakeUpElement("a","waves-effect waves-red btn-flat","");
+  buttom1.setAttribute("href","#!");
+  var btnicon1=MakeUpElement("i","material-icons","insert_emoticon");
+  buttom1.appendChild(btnicon1);
+  var buttom2=MakeUpElement("a","waves-effect waves-red btn-flat","");
+  buttom2.setAttribute("href","#!");
+  var btnicon2=MakeUpElement("i","material-icons","insert_emoticon");
+  buttom2.appendChild(btnicon2);
+  buttom.appendChild(buttom1);
+  buttom.appendChild(buttom2);
 
   temp.appendChild(icon);
   temp.appendChild(title);
-  temp.appendChild(description);
+  temp.appendChild(des1);
+  temp.appendChild(des2);
   temp.appendChild(buttom);
 
   return temp;
 }
 
+/*
+make up quest
+<li>
+          <div class="collapsible-header">
+            <i class="material-icons">filter_drama</i>
+            任务1</div>
+          <div class="collapsible-body">
+            <a></a>
+            <div>这是第一行说明</div>
+            <div>这是第二行说明</div>
+            <div href="#!" class="waves-effect waves-red btn-flat"><i class="material-icons">stars</i></div>
+            <div href="#!" class="waves-effect waves-red btn-flat"><i class="material-icons">stars</i></div>
+          </div>
+</li>
+*/
+
+function AddTask(_id,_title,_point,_description,_time){
+  var temp=MakeUpTask(_title,"filter_drama",_point,_description,_time)
+  temp.setAttribute("id","quest"+_id);
+  task_Renderer.appendChild(temp);
+}
+
+function MakeUpTask(_title,_icon,_point,_description,_time){
+  var temp=MakeUpElement("li","","");
+
+  var icon=MakeUpElement("i","material-icons",_icon);
+  var title=MakeUpElement("div","collapsible-header","");
+  var text=document.createTextNode(_title);
+  title.appendChild(icon);
+  title.appendChild(text)
+
+  var description=MakeUpElement("div","collapsible-body","");
+  var des1=MakeUpElement("div","","奖励点数"+_point);
+  var des2=MakeUpElement("div","",_description);
+  var des3=MakeUpElement("div","",_time);
+  description.appendChild(des1);
+  description.appendChild(des2);
+  description.appendChild(des3);
+
+  var buttom1=MakeUpElement("div","waves-effect waves-red btn-flat","");
+  buttom1.setAttribute("href","#!");
+  var btnicon1=MakeUpElement("i","material-icons","insert_emoticon");
+  buttom1.appendChild(btnicon1);
+  var buttom2=MakeUpElement("div","waves-effect waves-red btn-flat","");
+  buttom2.setAttribute("href","#!");
+  var btnicon2=MakeUpElement("i","material-icons","insert_emoticon");
+  buttom2.appendChild(btnicon2);
+  description.appendChild(buttom1);
+  description.appendChild(buttom2);
+
+  temp.appendChild(title);
+  temp.appendChild(description);
+  
+
+  return temp;
+}
 
